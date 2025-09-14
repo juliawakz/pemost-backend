eea0eb558dd074709f5bf0eafda5ee478429c832
juliawakz <gitjulia650@gmail.com>
2025-09-14 19:54:34 +0300

    Fix choices selection

--- backend/users/choices.py
+++ backend/users/choices.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class RoleChoices(TextChoices):
-    SYSTEM_ADMIN = "SYSTEM_ADMIN", _("system admin")
-    SUPER_EXTENSION = "SUPER_EXTENSION", _("super extension")
+    FARMER = "FARMER", _("farmer")
     AGRODEALER = "AGRODEALER", _("agrodealer")
     E_EXTENSION = "E_EXTENSION", _("e-extension")
-    FARMER = "FARMER", _("farmer")
+    SUPER_EXTENSION = "SUPER_EXTENSION", _("super extension")
+    SYSTEM_ADMIN = "SYSTEM_ADMIN", _("system admin")
--- backend/users/views/choices.py
+++ backend/users/views/choices.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from users.choices import RoleChoices
 from users.serializers.choices import RoleChoiceSerializer
 
@@ -13,9 +14,21 @@ class RoleChoicesView(APIView):
     serializer_class = RoleChoiceSerializer
 
     def get(self, request, *args, **kwargs):
+        u = request.user
         choices = [
             {"value": choice.value, "label": choice.label}
             for choice in RoleChoices
         ]
+        if u.role == RoleChoices.SUPER_EXTENSION:
+            choices = choices[:-2]
+        elif u.role == RoleChoices.E_EXTENSION:
+            choices = choices[:-3]
+        elif u.role == RoleChoices.AGRODEALER or u.role == RoleChoices.FARMER:
+            return Response(
+                {
+                    "Error": "You are not allowed to access this resource"
+                },
+                status=status.HTTP_403_FORBIDDEN
+            )
         serializer = RoleChoiceSerializer(choices, many=True)
         return Response(serializer.data)
